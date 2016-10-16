b1708f9173cf8c184eab031d3edd7572854cc67e
igushev <igushev@gmail.com>
2016-10-16 00:56:47 -0700

    Adding AllPointsQueue.

--- YuRouter/points_queue.py
+++ YuRouter/points_queue.py
@@ -1,3 +1,6 @@
+import datetime
+
+
 class PointsQueueInterface(object):
   """Abstract class which maintains points queue for pushing."""
 
@@ -48,3 +51,48 @@ class OneByOnePointsQueueGenerator(PointsQueueGeneratorInterface):
   
   def Generate(self, points):
     return OneByOnePointsQueue(points)
+
+
+class AllPointsQueue(PointsQueueInterface):
+  
+  def __init__(self, points, cut_off_multiplier):
+    self._points = points
+    self._cut_off_multiplier = cut_off_multiplier
+  
+  def HasPoints(self):
+    return bool(self._points)
+
+  def GetPushPoints(self, day_visit_parameterss):
+    total_timedelta = datetime.timedelta()
+    for day_visit_parameters in day_visit_parameterss:
+      total_timedelta += (
+          day_visit_parameters.end_datetime -
+          day_visit_parameters.start_datetime -
+          datetime.timedelta(hours=day_visit_parameters.lunch_hours))
+    total_hours = total_timedelta.total_seconds() / 60. / 60.
+    cut_off_hours = total_hours * self._cut_off_multiplier
+    
+    points_hours = 0.
+    for cut_off_i in range(len(self._points)):
+      points_hours += self._points[cut_off_i].duration
+      if points_hours >= cut_off_hours:
+        break
+    
+    push_points = self._points[:cut_off_i+1]
+    self._points = self._points[cut_off_i+1:]
+    return push_points
+  
+  def AddBackToQueue(self, points_left):
+    self._points = points_left + self._points
+
+  def GetPointsLeft(self):
+    return self._points
+
+
+class AllPointsQueueGenerator(PointsQueueGeneratorInterface):
+
+  def __init__(self, cut_off_multiplier):
+    self._cut_off_multiplier = cut_off_multiplier
+
+  def Generate(self, points):
+    return AllPointsQueue(points, self._cut_off_multiplier)
--- YuRouter/points_queue_test.py
+++ YuRouter/points_queue_test.py
@@ -1,15 +1,31 @@
+import datetime
 import os
 import unittest
 
 import Yusi
+from Yusi.YuPoint.city_visit import DayVisitParameters
 from Yusi.YuPoint.read_csv import ReadCSVToDict
-from Yusi.YuRouter.points_queue import OneByOnePointsQueueGenerator
+from Yusi.YuPoint.test_utils import MockCoordinates
 from Yusi.YuRouter.day_visit_cost_calculator_interface import DayVisitCostCalculatorInterface
+from Yusi.YuRouter.points_queue import OneByOnePointsQueueGenerator,\
+  AllPointsQueueGenerator
 
 
 class MockDayVisitCostCalculator(DayVisitCostCalculatorInterface):
   def __init__(self):
     pass
+
+
+def GetDayVisitParameterss(first_day, last_day):
+  def GetDayVisitParameters(day):
+    return DayVisitParameters(
+        start_datetime=datetime.datetime(2015, 7, day, 10, 0, 0),
+        end_datetime=datetime.datetime(2015, 7, day, 15, 0, 0),
+        lunch_start_datetime=datetime.datetime(2015, 7, day, 14, 0, 0),
+        lunch_hours=1.,
+        start_coordinates=MockCoordinates('Hotel'),
+        end_coordinates=MockCoordinates('Hotel'))
+  return [GetDayVisitParameters(day) for day in range(first_day, last_day)]
 
 
 class OneByOnePointsQueueTest(unittest.TestCase):
@@ -28,12 +44,7 @@ class OneByOnePointsQueueTest(unittest.TestCase):
 
     points_queue = OneByOnePointsQueueGenerator().Generate(points)
     self.assertTrue(points_queue.HasPoints())
-    self.assertEqual([self.points['Golden Gate Bridge'],
-                      self.points['Ferry Building'],
-                      self.points['Pier 39'],
-                      self.points['Union Square'],
-                      self.points['Twin Peaks']],
-                     points_queue.GetPointsLeft())
+    self.assertEqual(points, points_queue.GetPointsLeft())
     
     self.assertEqual([self.points['Golden Gate Bridge']],
                      points_queue.GetPushPoints(day_visit_parameterss))
@@ -79,6 +90,72 @@ class OneByOnePointsQueueTest(unittest.TestCase):
     points_queue.GetPushPoints(day_visit_parameterss)
     points_queue.GetPushPoints(day_visit_parameterss)
     self.assertEqual([self.points['Twin Peaks']],
+                     points_queue.GetPushPoints(day_visit_parameterss))
+    self.assertFalse(points_queue.HasPoints())
+    self.assertEqual([], points_queue.GetPointsLeft())
+
+
+class AllPointsQueueTest(unittest.TestCase):
+  
+  def setUp(self):
+    self.points = ReadCSVToDict(
+        os.path.join(Yusi.GetYusiDir(), 'YuPoint', 'test_sf_1.csv'))
+
+  def testGeneral(self):
+    day_visit_parameterss = GetDayVisitParameterss(1, 3)
+    points = [self.points['Golden Gate Bridge'],
+              self.points['Ferry Building'],
+              self.points['Pier 39'],
+              self.points['Union Square'],
+              self.points['Lombard Street'],
+              self.points['Coit Tower'],
+              self.points['Att Park'],
+              self.points['Alcatraz Island'],
+              self.points['Golden Gate Park'],
+              self.points['De Young Museum']]
+    
+    points_queue = AllPointsQueueGenerator(1.2).Generate(points)
+    self.assertTrue(points_queue.HasPoints())
+    self.assertEqual(points, points_queue.GetPointsLeft())
+
+    self.assertEqual([self.points['Golden Gate Bridge'],
+                      self.points['Ferry Building'],
+                      self.points['Pier 39'],
+                      self.points['Union Square'],
+                      self.points['Lombard Street'],
+                      self.points['Coit Tower'],
+                      self.points['Att Park'],
+                      self.points['Alcatraz Island']],
+                     points_queue.GetPushPoints(day_visit_parameterss))
+    self.assertTrue(points_queue.HasPoints())
+    self.assertEqual([self.points['Golden Gate Park'],
+                      self.points['De Young Museum']],
+                     points_queue.GetPointsLeft())
+
+    self.assertEqual([self.points['Golden Gate Park'],
+                      self.points['De Young Museum']],
+                     points_queue.GetPushPoints(day_visit_parameterss))
+    self.assertFalse(points_queue.HasPoints())
+    self.assertEqual([], points_queue.GetPointsLeft())
+
+  def testLargeCutOffMultiplier(self):
+    day_visit_parameterss = GetDayVisitParameterss(1, 3)
+    points = [self.points['Golden Gate Bridge'],
+              self.points['Ferry Building'],
+              self.points['Pier 39'],
+              self.points['Union Square'],
+              self.points['Lombard Street'],
+              self.points['Coit Tower'],
+              self.points['Att Park'],
+              self.points['Alcatraz Island'],
+              self.points['Golden Gate Park'],
+              self.points['De Young Museum']]
+    
+    points_queue = AllPointsQueueGenerator(2.0).Generate(points)
+    self.assertTrue(points_queue.HasPoints())
+    self.assertEqual(points, points_queue.GetPointsLeft())
+
+    self.assertEqual(points,
                      points_queue.GetPushPoints(day_visit_parameterss))
     self.assertFalse(points_queue.HasPoints())
     self.assertEqual([], points_queue.GetPointsLeft())

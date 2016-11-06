ce0e6dc208b51a25ceacfc6d453b29e877aa61a9
igushev <igushev@gmail.com>
2016-11-06 01:56:33 -0700

    Adding TaskManager

---
+++ YuUtils/task_utils.py
@@ -0,0 +1,82 @@
+from collections import namedtuple
+from datetime import datetime 
+from multiprocessing import Process, Pipe
+from threading import Lock, Timer
+import uuid
+
+from Yusi.YuUtils.misc_utils import WeakBoundMethod, Synchronized
+
+
+TaskWorkerContext = namedtuple(
+    'TaskWorkerContext', 'process manager_conn last_read')
+
+
+class TaskWorkerInterface(object):
+
+  def Start(self):
+    raise NotImplemented()
+
+
+class TaskWorkerGeneratorInterface(object):
+
+  def Generate(self, worker_conn):
+    raise NotImplemented()
+
+
+class TaskManager(object):
+  
+  def __init__(self, task_worker_generator, idle_seconds_terminate):
+    self.task_worker_generator = task_worker_generator
+    self.idle_seconds_terminate = idle_seconds_terminate
+    self.worker_process_dict = {}
+    self.timer = Timer(self.idle_seconds_terminate,
+                       WeakBoundMethod(self, TaskManager._CleanUp))
+    self.timer.start()
+    self.lock = Lock()
+  
+  def __del__(self):
+    self.timer.cancel()
+    self._CleanUp(start_over=False)
+  
+  @Synchronized()
+  def Start(self, *args, **kwargs):
+    manager_conn, worker_conn = Pipe(duplex=False)
+    task_worker = self.task_worker_generator.Generate(worker_conn)
+    worker_process_id = uuid.uuid1()
+    worker_process = Process(target=task_worker.Start, args=args, kwargs=kwargs)
+    worker_process.start()
+    self.worker_process_dict[worker_process_id] = (
+        TaskWorkerContext(worker_process, manager_conn, datetime.now()))
+    return worker_process_id
+
+  @Synchronized()
+  def Read(self, worker_process_id):
+    worker_context = self.worker_process_dict.get(worker_process_id)
+    if not worker_context:
+      raise AssertionError(
+          'No task with id %s. It might have been completed or expired.' %
+          worker_process_id)
+    while worker_context.manager_conn.poll():
+      res, done = worker_context.manager_conn.recv()
+      yield res
+      if done:
+        worker_context.process.join()
+        del self.worker_process_dict[worker_process_id]
+        return
+    self.worker_process_dict[worker_process_id] = (
+        TaskWorkerContext(worker_context.process, worker_context.manager_conn,
+                          datetime.now()))
+
+  @Synchronized()
+  def _CleanUp(self, start_over=True):
+    worker_process_id_list = list(self.worker_process_dict.iterkeys()) 
+    for worker_process_id in worker_process_id_list:
+      worker_context = self.worker_process_dict[worker_process_id]
+      if ((datetime.now() - worker_context.last_read).total_seconds() >
+          self.idle_seconds_terminate):
+        worker_context.process.terminate()
+        del self.worker_process_dict[worker_process_id]
+    if start_over:
+      self.timer = Timer(self.idle_seconds_terminate,
+                         WeakBoundMethod(self, TaskManager._CleanUp))
+      self.timer.start()
---
+++ YuUtils/task_utils_test.py
@@ -0,0 +1,96 @@
+import time
+import unittest
+from Yusi.YuUtils.task_utils import TaskWorkerGeneratorInterface, TaskManager
+
+
+def Sqr(num):
+  return num*num
+
+
+class SqrTaskWorker(TaskWorkerGeneratorInterface):
+  
+  def __init__(self, worker_conn):
+    self.worker_conn = worker_conn
+  
+  def Start(self, num_list, sleep):
+    for i, num in enumerate(num_list):
+      time.sleep(sleep)
+      self.worker_conn.send((Sqr(num), i == len(num_list)-1))
+
+    
+class SqrTaskWorkerGenerator(TaskWorkerGeneratorInterface):
+
+  def Generate(self, worker_conn):
+    return SqrTaskWorker(worker_conn)
+
+
+class TaskUtilsTest(unittest.TestCase):
+  
+  def testGeneral(self):
+    sqr_task_worker_general = SqrTaskWorkerGenerator() 
+    task_manager = TaskManager(sqr_task_worker_general, 1.0)
+    
+    input_1 = range(0, 11, 2)
+    results_1 = [Sqr(num) for num in input_1]
+    input_2 = range(1, 8, 2)
+    results_2 = [Sqr(num) for num in input_2]
+    id_1 = task_manager.Start(input_1, 0.2)
+    id_2 = task_manager.Start(input_2, 0.2)
+
+    time.sleep(0.5)
+    read_list_1_1 = list(task_manager.Read(id_1))
+    read_list_2_1 = list(task_manager.Read(id_2))
+    self.assertTrue(1 <= len(read_list_1_1) <= 3)
+    self.assertEqual(results_1[:len(read_list_1_1)], read_list_1_1)
+    self.assertTrue(1 <= len(read_list_2_1) <= 3)
+    self.assertEqual(results_2[:len(read_list_2_1)], read_list_2_1)
+    results_1 = results_1[len(read_list_1_1):]
+    results_2 = results_2[len(read_list_2_1):]
+    
+    time.sleep(0.5)
+    read_list_1_2 = list(task_manager.Read(id_1))
+    read_list_2_2 = list(task_manager.Read(id_2))
+    self.assertTrue(1 <= len(read_list_1_2) <= 3)
+    self.assertEqual(results_1[:len(read_list_1_2)], read_list_1_2)
+    self.assertTrue(1 <= len(read_list_2_2) <= 3)
+    self.assertEqual(results_2, read_list_2_2)
+    results_1 = results_1[len(read_list_1_2):]
+    results_2 = []
+
+    time.sleep(0.5)
+    read_list_1_3 = list(task_manager.Read(id_1))
+    self.assertRaisesRegexp(
+        AssertionError, 'No task with id %s' % id_2,
+        list, task_manager.Read(id_2))
+    self.assertTrue(1 <= len(read_list_1_3) <= 3)
+    self.assertEqual(results_1, read_list_1_3)
+    results_1 = []
+    
+    time.sleep(0.5)
+    self.assertRaisesRegexp(
+        AssertionError, 'No task with id %s' % id_1,
+        list, task_manager.Read(id_1))
+    self.assertRaisesRegexp(
+        AssertionError, 'No task with id %s' % id_2,
+        list, task_manager.Read(id_2))
+
+  def testCleanUp(self):
+    sqr_task_worker_general = SqrTaskWorkerGenerator() 
+    task_manager = TaskManager(sqr_task_worker_general, 0.5)
+    
+    input_1 = range(0, 3, 2)
+    input_2 = range(1, 4, 2)
+    id_1 = task_manager.Start(input_1, 0.2)
+    id_2 = task_manager.Start(input_2, 0.2)
+
+    time.sleep(1.5)
+    self.assertRaisesRegexp(
+        AssertionError, 'No task with id %s' % id_1,
+        list, task_manager.Read(id_1))
+    self.assertRaisesRegexp(
+        AssertionError, 'No task with id %s' % id_2,
+        list, task_manager.Read(id_2))
+
+
+if __name__ == '__main__':
+    unittest.main()

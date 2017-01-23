09ecda8be97d3639b92b3471c8c726dec51dabc9
igushev <igushev@gmail.com>
2017-01-23 01:23:10 -0800

    Support Abstract classes and None object in JSONDecorator

--- YuUtils/json_utils.py
+++ YuUtils/json_utils.py
@@ -1,6 +1,14 @@
+import copy
 import json
+import os
+from datetime import datetime
+
     
-    
+DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
+MODULE_FIELD = '__module__'
+CLASS_FIELD = '__class__'
+
+
 class JSONObjectInterface(object):
   
   def ToSimple(self, obj):
@@ -10,7 +18,7 @@ class JSONObjectInterface(object):
     raise NotImplemented()
 
 
-class JSONSimple(object):
+class JSONSimple(JSONObjectInterface):
   
   def ToSimple(self, obj):
     return obj
@@ -19,7 +27,7 @@ class JSONSimple(object):
     return simple
   
 
-class JSONInt(object):
+class JSONInt(JSONObjectInterface):
   
   def ToSimple(self, obj):
     assert isinstance(obj, int)
@@ -30,7 +38,18 @@ class JSONInt(object):
     return int(simple)
 
 
-class JSONObject(object):
+class JSONDateTime(JSONObjectInterface):
+  
+  def ToSimple(self, obj):
+    assert isinstance(obj, datetime)
+    return obj.strftime(DATETIME_FORMAT)
+  
+  def FromSimple(self, simple):
+    assert isinstance(simple, basestring)
+    return datetime.strptime(simple, DATETIME_FORMAT)
+
+
+class JSONObject(JSONObjectInterface):
   
   def __init__(self, cls):
     self._cls = cls
@@ -45,6 +64,7 @@ class JSONObject(object):
 class JSONList(object):
   
   def __init__(self, json_obj):
+    assert isinstance(json_obj, JSONObjectInterface)
     self._json_obj = json_obj
     
   def ToSimple(self, obj):
@@ -59,6 +79,8 @@ class JSONList(object):
 class JSONDict(object):
   
   def __init__(self, key_json_obj, value_json_obj):
+    assert isinstance(key_json_obj, JSONObjectInterface)
+    assert isinstance(value_json_obj, JSONObjectInterface)
     self._key_json_obj = key_json_obj
     self._value_json_obj = value_json_obj
   
@@ -76,12 +98,15 @@ class JSONDict(object):
 
 
 def ToSimple(self):
-  cls = self.__class__
+  obj_cls = self.__class__
   json_default = JSONSimple()
   simple = dict()
   for key, value in self.__dict__.iteritems():
-    json_obj = cls.desc_dict.get(key, json_default)
-    simple[key] = json_obj.ToSimple(value)
+    json_obj = obj_cls.desc_dict.get(key, json_default)
+    simple[key] = json_obj.ToSimple(value) if value is not None else None
+  if obj_cls.inherited:
+    simple[MODULE_FIELD] = obj_cls.__module__
+    simple[CLASS_FIELD] = obj_cls.__name__
   return simple
 
 
@@ -91,12 +116,20 @@ def ToJSON(self):
 
 @classmethod
 def FromSimple(cls, simple):
+  if cls.inherited:
+    obj_cls = getattr(os.sys.modules[simple[MODULE_FIELD]], simple[CLASS_FIELD])
+  else:
+    obj_cls = cls
+
   json_default = JSONSimple()
   obj_dict = dict()
   for key, value in simple.iteritems():
-    json_obj =  cls.desc_dict.get(key, json_default)
-    obj_dict[str(key)] = json_obj.FromSimple(value)
-  obj = cls.__new__(cls)
+    if key in [MODULE_FIELD, CLASS_FIELD]:
+      continue
+    json_obj =  obj_cls.desc_dict.get(key, json_default)
+    obj_dict[str(key)] = (json_obj.FromSimple(value)
+                          if value is not None else None)
+  obj = obj_cls.__new__(obj_cls)
   obj.__dict__ = obj_dict 
   return obj
 
@@ -108,11 +141,27 @@ def FromJSON(cls, data):
 
 class JSONDecorator(object):
   
-  def __init__(self, desc_dict=None):
+  def __init__(self, desc_dict=None, inherited=False):
     self.desc_dict = desc_dict or {}
+    self.inherited = inherited
 
   def __call__(self, cls):
-    cls.desc_dict = self.desc_dict
+    desc_dict = copy.copy(self.desc_dict)
+    inherited = self.inherited
+
+    curr_cls = cls
+    while True:
+        bases = curr_cls.__bases__
+        assert len(bases) == 1, 'Multiple inheritance is not supported!'
+        base = bases[0]
+        if getattr(base, 'desc_dict', None) is None:
+          break
+        desc_dict.update(base.desc_dict)
+        inherited = inherited or base.inherited
+        curr_cls = base
+    
+    cls.desc_dict = desc_dict
+    cls.inherited = inherited
     cls.ToSimple = ToSimple
     cls.ToJSON = ToJSON
     cls.FromSimple = FromSimple
--- YuUtils/json_utils_test.py
+++ YuUtils/json_utils_test.py
@@ -3,19 +3,23 @@ import unittest
 from Yusi.YuUtils import json_utils
 from Yusi.YuUtils.hash_utils import HashKey
 from Yusi.YuUtils.repr_utils import Repr
+from datetime import datetime
 
 
 @json_utils.JSONDecorator(
-    {'_int_field': json_utils.JSONInt()})
+    {'_int_field': json_utils.JSONInt(),
+     'datetime_field': json_utils.JSONDateTime()})
 class WithFields(object):
   
-  def __init__(self, int_field, float_field, string_field):
+  def __init__(self, int_field, float_field, string_field, datetime_field):
     assert isinstance(int_field, int)
     assert isinstance(float_field, float)
     assert isinstance(string_field, basestring)
+    assert isinstance(datetime_field, datetime)
     self._int_field = int_field  # With underscore.
     self.float_field = float_field
     self._string_field = string_field  # With underscore.
+    self.datetime_field = datetime_field
 
   def HashKey(self):
     return HashKey(self.__dict__)
@@ -75,29 +79,181 @@ class WithNestedFields(object):
     return self.__dict__ == other.__dict__
 
 
-class JSONUtilsTest(unittest.TestCase):
+# We declare _nested_with_fields_2 to be WithFields but allow None.
+@json_utils.JSONDecorator(
+    {'_nested_with_fields_1': json_utils.JSONObject(WithFields),
+     '_nested_with_fields_2': json_utils.JSONObject(WithFields)})
+class WithNestedNoneObjectsFields(object):
+
+  def __init__(self, nested_with_fields_1, nested_with_fields_2):
+    assert isinstance(nested_with_fields_1, WithFields)
+    if nested_with_fields_2 is not None:
+      assert isinstance(nested_with_fields_2, WithFields)
+    # With underscore.
+    self._nested_with_fields_1 = nested_with_fields_1
+    # With underscore.
+    self._nested_with_fields_2 = nested_with_fields_2
+
+  def HashKey(self):
+    return HashKey(self.__dict__)
+
+  def __repr__(self):
+    return Repr(self)
+
+  def __eq__(self, other):
+    return self.__dict__ == other.__dict__
+
+
+@json_utils.JSONDecorator(inherited=True)
+class Level1(object):
+
+  def __init__(self, var1):
+    assert isinstance(var1, float)
+    self.var1 = var1
+
+  def HashKey(self):
+    return HashKey(self.__dict__)
+
+  def __repr__(self):
+    return Repr(self)
+
+  def __eq__(self, other):
+    return self.__dict__ == other.__dict__
+
+
+@json_utils.JSONDecorator()
+class Level2(Level1):
+
+  def __init__(self, var1, var2):
+    super(Level2, self).__init__(var1)
+    assert isinstance(var2, float)
+    self.var2 = var2
+
+  def HashKey(self):
+    return HashKey(self.__dict__)
+
+  def __repr__(self):
+    return Repr(self)
+
+  def __eq__(self, other):
+    return self.__dict__ == other.__dict__
+
+
+@json_utils.JSONDecorator()
+class Level3A(Level2):
+
+  def __init__(self, var1, var2, var3):
+    super(Level3A, self).__init__(var1, var2)
+    assert isinstance(var3, float)
+    self.var3 = var3
+
+  def HashKey(self):
+    return HashKey(self.__dict__)
+
+  def __repr__(self):
+    return Repr(self)
+
+  def __eq__(self, other):
+    return self.__dict__ == other.__dict__
+
+
+@json_utils.JSONDecorator()
+class Level3B(Level2):
+
+  def __init__(self, var1, var2, var3):
+    super(Level3B, self).__init__(var1, var2)
+    assert isinstance(var3, basestring)
+    self.var3 = var3
+
+  def HashKey(self):
+    return HashKey(self.__dict__)
+
+  def __repr__(self):
+    return Repr(self)
+
+  def __eq__(self, other):
+    return self.__dict__ == other.__dict__
+
+
+@json_utils.JSONDecorator(
+    {'diff_level_list': json_utils.JSONList(json_utils.JSONObject(Level1))})
+class WithDifferentLevelList(object):
   
-  def testGeneral(self):
-    obj = WithNestedFields(
-        WithFields(3, 5., u'seven'),
-        WithListAndDict(
-            [WithFields(2, 4., u'six'),
-             WithFields(8, 10., u'twelve')],
-            {u'one': WithFields(15, 17., u'nineteen'),
-             u'two': WithFields(21, 23., u'twenty five')}),
-        None, True)
+  def __init__(self, diff_level_list):
+    assert isinstance(diff_level_list, list)
+    self.diff_level_list = diff_level_list
+
+  def HashKey(self):
+    return HashKey(self.__dict__)
+
+  def __repr__(self):
+    return Repr(self)
+
+  def __eq__(self, other):
+    return self.__dict__ == other.__dict__
+
+
+class JSONUtilsTest(unittest.TestCase):
+
+  def AssertToFrom(self, obj, obj_cls):
     obj_simple = obj.ToSimple()
-    obj_from_simple = WithNestedFields.FromSimple(obj_simple)
+    obj_from_simple = obj_cls.FromSimple(obj_simple)
     self.assertEqual(obj.HashKey(), obj_from_simple.HashKey())
     self.assertEqual(repr(obj), repr(obj_from_simple))
     self.assertEqual(obj, obj_from_simple)
     
     obj_json = obj.ToJSON()
-    obj_from_json = WithNestedFields.FromJSON(obj_json)
+    obj_from_json = obj_cls.FromJSON(obj_json)
     self.assertEqual(obj.HashKey(), obj_from_json.HashKey())
     self.assertEqual(repr(obj), repr(obj_from_json))
     self.assertEqual(obj, obj_from_json)
 
+  def testGeneral(self):
+    datetime_1 = datetime(1986, 5, 22, 13, 0, 0)
+    datetime_2 = datetime(1986, 8, 21, 13, 0, 0)
+    datetime_3 = datetime(2014, 1, 3, 9, 54, 0)
+    obj = WithNestedFields(
+        WithFields(3, 5., u'seven', datetime_1),
+        WithListAndDict(
+            [WithFields(2, 4., u'six', datetime_2),
+             WithFields(8, 10., u'twelve', datetime_2)],
+            {u'one': WithFields(15, 17., u'nineteen', datetime_3),
+             u'two': WithFields(21, 23., u'twenty five', datetime_3)}),
+        None, True)
+    self.AssertToFrom(obj, WithNestedFields)
+
+  def testInheritance(self):
+    level3a = Level3A(1., 3., 5.)
+    self.AssertToFrom(level3a, Level3A)
+    # Can call using parent class.
+    self.AssertToFrom(level3a, Level1)
+    level3b = Level3B(2., 4., u'six')
+    self.AssertToFrom(level3b, Level3B)
+    # Can call using parent class.
+    self.AssertToFrom(level3b, Level1)
+
+    # List of mixed instances of derivative classes.    
+    level3a_2 = Level3A(7., 9., 11.)
+    level3b_2 = Level3B(8., 10., u'twelve')
+    obj = WithDifferentLevelList(
+        [level3a, level3b, level3a_2, level3b_2])
+    self.AssertToFrom(obj, WithDifferentLevelList)
+
+  def testNestedObjectNone(self):
+    datetime_1 = datetime(1986, 5, 22, 13, 0, 0)
+    datetime_2 = datetime(1986, 8, 21, 13, 0, 0)
+    datetime_3 = datetime(2014, 1, 3, 9, 54, 0)
+    # Both fields are set.
+    obj = WithNestedNoneObjectsFields(
+        WithFields(3, 5., u'seven', datetime_1),
+        WithFields(2, 4., u'six', datetime_2))
+    self.AssertToFrom(obj, WithNestedNoneObjectsFields)
+
+    # Second field is None.
+    obj = WithNestedNoneObjectsFields(
+        WithFields(15, 17., u'nineteen', datetime_3), None)
+    self.AssertToFrom(obj, WithNestedNoneObjectsFields)
+    
 
 if __name__ == '__main__':
     unittest.main()
